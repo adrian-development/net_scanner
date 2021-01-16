ff6e3ecd1252b39343391df39017c838bf9b077b
Adrian Jäger <jaegerad74878@th-nuernberg.de>
2021-01-16 19:13:28 +0100

    Added Intervall argument, refactoring

--- plotit.sh
+++ plotit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#Konstante
-FILENAME="scan.dat"
+#Zu bearbeitende Datei
+filename="$1"
 
 #Start Konsolenoutput
-echo Starting to Plot File
+echo -e "\nStarting to Plot File ${filename}:"
 
 #Entnehme alle Hosts aus erster Zeile, trenne erste Spalte da dort "Zeit" steht
-HostList="$(head -n 1 $FILENAME | cut -d ' ' -f 2- )"
+HostList="$(head -n 1 "$filename" | cut -d ' ' -f 2- )"
 
 #Debug, zeige Hostliste
-echo Hosts: $HostList
+echo Hosts: "$HostList"
 
 #Start bei Spalte 2 da Spalte 1 für Zeiten
 column_count=1
@@ -20,30 +20,37 @@ plot_arg="plot "
 for host in $HostList
 do
     ((column_count++))
-    plot_arg="${plot_arg} \"$FILENAME\" using 1:$column_count title \"$host\" with linespoints,"
+    plot_arg="${plot_arg} \"$filename\" using 1:$column_count title \"$host\" with linespoints,"
 done
 #Debug, zeige Kommando
 echo "$plot_arg"
-echo "SPaltenzahl: $column_count"
+echo "Spaltenzahl: $column_count"
 
 #Erfasse Zeitrange, da gnuplot bei Zeiten auf x-Achse kein Autoscale verwenden kann
-zeit_start=$(sed -n 2p $FILENAME | cut -d ' ' -f 1)
-zeit_end=$(tail -n 1 $FILENAME | cut -d ' ' -f 1)
+zeit_start=$(sed -n 2p "$filename" | cut -d ' ' -f 1)
+zeit_end=$(tail -n 1 "$filename" | cut -d ' ' -f 1)
 #Debug zeige Zeiten
-echo Start: $zeit_start Ende: $zeit_end
+echo "Start: $zeit_start Ende: $zeit_end"
 
 #Vorverarbeitung, fülle leere Felder da gnuplot damit nicht umgehen kann
 line_count=1
-while read line; do
+
+#Lese Datei line by line
+while read -r line; do
+    #Ermittle Spaltenzahl in dieser Zeile
     word_count=$( echo "$line" | wc -w)
+
+    #Baue neue Zeile, füge fehlende Felder mit -100 ein
     while [[ $word_count < $column_count ]] ; do
         line=$( printf '%s%20s' "$line" "-100" )
         ((word_count++))
     done
-    sed -i "${line_count}s/.*/$line/" $FILENAME
-    ((line_count++))
-done < $FILENAME
 
+    #Ersetze Zeile durch bearbeitete Zeile
+    sed -i "${line_count}s/.*/$line/" "$filename"
+
+    ((line_count++))
+done < "$filename"
 
 
 #Parametrisiere Gnuplot mit HERE Dokument und obigem Kommando
@@ -59,7 +66,7 @@ set xrange ["$zeit_start":"$zeit_end"]
 $plot_arg
 PLOT
 
-echo Fertig
+echo "Plot fertig"
 
 exit 0
 
--- scanner.sh
+++ scanner.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo -e "Netzwerkscanner gestartet \nWarnung admin rechte zum generieren von ARP-Packages benötigt\n"
-
-#Konstanten
-OUTPUT_FILE="./scan.dat"
+echo -e "Netzwerkscanner gestartet \nWarnung: Admin Rechte zum generieren von ARP-Packages benötigt\n"
 
 #Variablen initialisieren
 IP_list=
 OutputLine=
+scan_mode=1
+intervall=10
 
 
 #Optionen Ermitteln
@@ -16,14 +15,25 @@ while [[ ${1::1} == '-' ]] ; do
         --ping|-p)
             echo "Extra Pings ausgewählt"
             shift
-            while [ "${1::1}" != '-' -a -n "${1::1}" ] ; do
+            while [[ "${1::1}" != '-' ]] && [[ -n "${1::1}" ]] ; do
                 IP_list=$( printf '%s%20s' "${IP_list}" "${1}")
                 shift
             done
             ;;
 
+        --interval|-i)
+            echo "Intervall von $1 ausgwählt"
+            intervall="$1"
+            ;;
+
+        --auswertung|-a)
+            echo "Auswertungsmodus ausgewählt"
+            shift
+            FILENAME="$1"
+            scan_mode=
+            ;;
         *)
-            echo "Unbekannte Option" $1
+            echo "Unbekannte Option" "$1"
             ;;
     esac
     shift
@@ -31,7 +41,7 @@ while [[ ${1::1} == '-' ]] ; do
 done
 
 #Prüfe Vorausetzungen
-if ! command -v arp-scan &> /dev/null
+if [[ $scan_mode -eq "1" ]] && ! command -v arp-scan &> /dev/null
 then
     read -p "arp-scan could not be found, install now? (y/n)" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]
@@ -49,91 +59,103 @@ then
     fi
 fi
 
-#Zum Testen: Einmale Host-String anlegen und deise zyklisch anpingen
-#IP_arp=$(sudo arp-scan --localnet --numeric --quiet --ignoredups --bandwidth 1000000 | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
-#IP_list="${IP_list} ${IP_arp}"
-#echo Ping geht an $IP_list
+#Prüfe ob ein Netzwerkscann ausgeführt werden soll
+if [[ scan_mode -eq "1" ]]; then
 
-#Outputfile Anlegen 
-echo Zeit ${IP_list} >$OUTPUT_FILE
+    #Generiere Dateinamen
+    FILENAME=$(date +"scan_%F")
 
-#Zyklischer Aufruf
-while true ; do
+    #Outputfile Anlegen 
+    echo Zeit "${IP_list}" >"$FILENAME"
 
-    #Consol output
-    echo Next Scan:
+    #Ermögliche STRG+C zum beenden der Schleife
+    trap break INT
 
-    #Get time for Output File
-    OutputLine=$( printf '%-20s' "$(date +"%H:%M:%S")" )
+    #Zyklischer Aufruf
+    while true ; do
 
-    #Erkenne alle Geräte im Netzwerk mit arp-scan
-    IP_arp=$(sudo arp-scan --localnet --numeric --quiet --ignoredups --bandwidth 1000000 | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
-    #Alternative: nmap. Wesentlich mächtiger
-    #IP_list=$(nmap -nsP 192.168.178.0/24 2>/dev/null -oG - | grep "Up$" | awk '{printf "%s ", $2}')
+        #Consol output
+        echo Next Scan:
+
+        #Get time for Output File
+        OutputLine=$( printf '%-20s' "$(date +"%H:%M:%S")" )
+
+        #Erkenne alle Geräte im Netzwerk mit arp-scan
+        IP_arp=$(sudo arp-scan --localnet --numeric --quiet --ignoredups --bandwidth 1000000 | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
+        #Alternative: nmap. Wesentlich mächtiger
+        #IP_list=$(nmap -nsP 192.168.178.0/24 2>/dev/null -oG - | grep "Up$" | awk '{printf "%s ", $2}')
 
 
-    #Prüfe ob Host bereits in Liste enthalten, wenn nicht füge hinten an
-    for IP in $IP_arp
-    do
-        if [[ "$IP_list" != *"$IP"* ]]; then
-            IP_list=$(printf '%s%20s' "${IP_list}" "${IP}")
-        fi
+        #Prüfe ob Host bereits in Liste enthalten, wenn nicht füge hinten an
+        for IP in $IP_arp
+        do
+            if [[ "$IP_list" != *"$IP"* ]]; then
+                IP_list=$(printf '%s%20s' "${IP_list}" "${IP}")
+            fi
+        done
+
+        #Debug: Zeige liste
+        echo Hosts: "$IP_list"
+
+        #Ping an Netzwerkteilnehmer
+        for IP in $IP_list ;  do
+
+            #Output for Console
+            echo -n "Ping an ${IP}: "
+
+            #Sende einen Ping
+            ping_output=$(ping -q -n -w 1 -c 1 "${IP}")
+            #debug echo
+            #echo "${ping_output}"
+
+            #Auswertung ob Ping erfolgreich war
+            ping_ok=$(echo "${ping_output}" | grep "transmitted" | awk '{
+                #printf("1: %s, 2: %s  \n\n", $1, $4)
+                if ($1 == $4)
+                    print "ok"
+                else
+                    print "error" }')
+
+            #Zeige Ergebnis
+            #echo -n "${ping_ok} "      
+
+            #Zeige Ping Zeit wenn Ping erfolgreich war
+            if [[ $ping_ok == "ok" ]] ; then
+                ping_time=$(echo "${ping_output}" | grep "rtt" | perl -npe 's/.*=\s([0-9.]*)\/.*/$1\n/')
+                echo "$ping_time" ms
+
+                #Ausgabepuffer
+                OutputLine=$(printf '%s%20s' "${OutputLine}" "${ping_time}")
+            else   
+                echo "Error"
+
+                #Ausgabepuffer
+                OutputLine=$(printf '%s%20s' "${OutputLine}" "-100")
+            fi
+
+
+        done
+
+        #Save scan to File
+        echo "$OutputLine" >>"$FILENAME"
+
+        #Replace first Line to integrate new Hosts
+        first_line=$( printf '%-20s%s' "Zeit" "$IP_list" )
+        sed -i "1s/.*/$first_line/" "$FILENAME"
+
+        sleep "$intervall"
+
     done
 
-    #Debug: Zeige liste
-    echo Hosts: "$IP_list"
+    #Stop trap
+    trap - INT
 
-    #Ping an Netzwerkteilnehmer
-    for IP in $IP_list ;  do
-
-        #Output for Console
-        echo -n "Ping an ${IP}: "
-
-        #Sende einen Ping
-        ping_output=$(ping -q -n -w 1 -c 1 "${IP}")
-        #debug echo
-        #echo "${ping_output}"
-
-        #Auswertung ob Ping erfolgreich war
-        ping_ok=$(echo "${ping_output}" | grep "transmitted" | awk '{
-            #printf("1: %s, 2: %s  \n\n", $1, $4)
-            if ($1 == $4)
-                print "ok"
-            else
-                print "error" }')
-
-        #Zeige Ergebnis
-        #echo -n "${ping_ok} "      
-
-        #Zeige Ping Zeit wenn Ping erfolgreich war
-        if [[ $ping_ok == "ok" ]] ; then
-            ping_time=$(echo "${ping_output}" | grep "rtt" | perl -npe 's/.*=\s([0-9.]*)\/.*/$1\n/')
-            echo $ping_time ms
-
-            #Ausgabepuffer
-            OutputLine=$(printf '%s%20s' "${OutputLine}" "${ping_time}")
-        else   
-            echo "Error"
-
-            #Ausgabepuffer
-            OutputLine=$(printf '%s%20s' "${OutputLine}" "-100")
-        fi
+fi
 
 
-    done
+#Starte Auswertung
+./plotit.sh "$FILENAME"
 
-    #Save scan to File
-    echo "$OutputLine" >>$OUTPUT_FILE
-
-    #Replace first Line to integrate new Hosts
-    first_line=$( printf '%-20s%s' "Zeit" "$IP_list" )
-    sed -i "1s/.*/$first_line/" $OUTPUT_FILE
-
-    sleep 1
-
-done
-
-
-echo ende
+echo Fertig
 
 exit 0
