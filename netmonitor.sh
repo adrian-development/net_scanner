fe6fba49190997b8a7491b79e81022059a716de8
Adrian Jäger <jaegerad74878@th-nuernberg.de>
2021-01-17 20:08:11 +0100

    refactoring

--- netmonitor.sh
+++ netmonitor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #----------------------------------------------------------
-#    File-name: <~/bin/hello.sh>#     Language: bash script
-#     Synopsis:
-#  Description: My first bash program
+#    File-name: netmonitor.sh
+#     Language: bash script
+#     Synopsis: netmonitor.sh [-p|--ping hosts] [-h|--help] [-i|--intervall wartezeit] [-a|--auswertung filename]
+#  Description: Ein tool zur Analyse des lokalen Netzwerks
 #      Project: Shell Script Programming Course
-#       Author: Michael.Zwanger@th-nuernberg.de
+#       Author: jaegerad74878@th-nuernberg.de
 #----------------------------------------------------------
-echo -e "Netzwerkscanner gestartet \nWarnung: Admin Rechte zum generieren von ARP-Packages benötigt\n"
 
 #Variablen initialisieren
 IP_list=
@@ -14,66 +14,120 @@ OutputLine=
 scan_mode=1
 intervall=10
 
+#Help Funktion
+function help ()
+{
+    cat <<HELPTEXT
+NAME
+    netmonitor.sh
+    Nimmt eine Analyse Ihres Netzwerks vor.
+SYNOPSIS
+    netmonitor.sh [-p|--ping hosts] [-h|--help] [-i|--intervall wartezeit] [-a|--auswertung filename]
+DESCRIPTION
+    Netmonitor ermittelt über einen ARP-Scan zyklisch sämtliche Geräte in Ihrem IPv4 Netzwerkes und pingt dieses an
+    um die performance über längere Zeitabschnitte zu protokollieren. Die Messdaten werden in einer separaten Datei
+    gespeichert und nach Ende des Scans graphisch dargestellt.
+    Die Messung wird durch STRG+C gestoppt
+EXAMPLES
+    netmonitor.sh: Startet die Aufzeichung im Standardintervall von 10 Sekunden
+    netmonitor.sh --ping www.google.de 141.75.201.12 --intervall 20: Pingt zusätzliche die google und die genannte IP
+    alle 20 Sekunden an.
+    netmonitor.sh --auswertung scan_2021-01-17: Startet keinen Scan sondern wertet eine bereits erstellt Datei aus
+AUTHOR
+    Adrian Jäger - jaegerad74878@th-nuernberg.de
+HELPTEXT
+
+    return
+}
 
 #Optionen Ermitteln
 while [[ ${1::1} == '-' ]] ; do
     case $1 in
         --ping|-p)
             echo "Extra Pings ausgewählt"
-            shift
-            while [[ "${1::1}" != '-' ]] && [[ -n "${1::1}" ]] ; do
-                IP_list=$( printf '%s%20s' "${IP_list}" "${1}")
+            #Hänge alle Angegebnen Ziele an IP Liste an
+            while [[ "${2::1}" != '-' ]] && [[ -n "${2::1}" ]] ; do
                 shift
+                IP_list=$( printf '%s%20s' "${IP_list}" "${1}")
             done
             ;;
 
         --interval|-i)
             shift
-            echo "Intervall von $1 ausgwählt"
-            intervall="$1"
+            #Prüfe ob Intervall eine Ganzzahl ist
+            if  [[ $1 =~ ^[0-9]+$ ]]; then
+                echo "Intervall von $1 ausgwählt"
+                intervall="$1"
+            else
+                echo "Invalides Intervall: $1"
+                exit 1
+            fi
             ;;
 
         --auswertung|-a)
-            echo "Auswertungsmodus ausgewählt"
             shift
-            FILENAME="$1"
-            scan_mode=
+            #Prüfe ob Datei vorhanden ist
+            if [[ "${1::1}" != '-' ]] && [[ -n "${1::1}" ]] && test -f "$1"; then
+                echo "Auswertungsmodus ausgewählt"
+                filename="$1"
+                scan_mode=0
+            else
+                echo "Invalide Datei: $1"
+                exit 1
+            fi
             ;;
+
+        --hepl|-h)
+            #Geben Manpage aus
+            help
+            exit 0
+            ;;
+
         *)
-            echo "Unbekannte Option" "$1"
+            echo "Unbekannte Option: $1"
+            exit 1
             ;;
     esac
     shift
 
 done
 
-#Prüfe Vorausetzungen
+#Prüfe Abhängigkeiten, biete installation bei fehlen an
 if [[ $scan_mode -eq "1" ]] && ! command -v arp-scan &> /dev/null
 then
-    read -p "arp-scan could not be found, install now? (y/n)" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
+    read -p "arp-scan nicht gefunden, jetzt installieren? (j/n)" -n 1 -r
+    if [[ $REPLY =~ ^[Jj]$ ]]
     then
         sudo apt-get install arp-scan
+    else
+        echo "Fehler: arp-scan nicht gefunden"
+        exit 1
     fi
 fi
 
 if ! command -v gnuplot &> /dev/null
 then
-    read -p "gnuplot could not be found, install now? (y/n)" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
+    read -p "gnuplot nicht gefunden, jetzt installieren? (j/n)" -n 1 -r
+    if [[ $REPLY =~ ^[Jj]$ ]]
     then
         sudo apt-get install gnuplot
+    else
+        echo "Fehler: gnuplot nicht gefunden"
+        exit 1
     fi
 fi
 
 #Prüfe ob ein Netzwerkscann ausgeführt werden soll
 if [[ scan_mode -eq "1" ]]; then
 
+    #Consolen Output
+    echo -e "Netzwerkscanner gestartet \nWarnung: Admin Rechte zum generieren von ARP-Packages benötigt\n"
+
     #Generiere Dateinamen
-    FILENAME=$(date +"scan_%F")
+    filename=$(date +"scan_%F")
 
     #Outputfile Anlegen 
-    echo Zeit "${IP_list}" >"$FILENAME"
+    echo Zeit "${IP_list}" >"$filename"
 
     #Ermögliche STRG+C zum beenden der Schleife
     trap break INT
@@ -82,7 +136,7 @@ if [[ scan_mode -eq "1" ]]; then
     while true ; do
 
         #Consol output
-        echo Next Scan:
+        echo -n "Nächster Scann beginnt: "
 
         #Get time for Output File
         OutputLine=$( printf '%-20s' "$(date +"%H:%M:%S")" )
@@ -144,11 +198,11 @@ if [[ scan_mode -eq "1" ]]; then
         done
 
         #Save scan to File
-        echo "$OutputLine" >>"$FILENAME"
+        echo "$OutputLine" >>"$filename"
 
         #Replace first Line to integrate new Hosts
         first_line=$( printf '%-20s%s' "Zeit" "$IP_list" )
-        sed -i "1s/.*/$first_line/" "$FILENAME"
+        sed -i "1s/.*/$first_line/" "$filename"
 
         sleep "$intervall"
 
@@ -161,7 +215,7 @@ fi
 
 
 #Starte Auswertung
-./plotit.sh "$FILENAME"
+./plotit.sh "$filename"
 
 echo Fertig
 
